431ccbb023d589eed4bf2e8f96742f9819827575
Joseph Mckenzi <josephmckenzieaz@yahoo.com>
2019-05-27 16:55:57 -0400

    got rid of more duplicated functions that were to close to the same and brought down the main function count to just the one

--- Teecee-Coder/Teecee-Coder/AppDelegate.swift
+++ Teecee-Coder/Teecee-Coder/AppDelegate.swift
@@ -14,7 +14,9 @@ import Foundation
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
     var statusBarItem: NSStatusItem!
-    
+    var api_endpoint = ""
+//    var pic_folder = ""
+    var puppy = ""
     func applicationDidFinishLaunching(_ aNotification: Notification) {
       
 //        if Reachability.isConnectedToNetwork(){
@@ -34,17 +36,17 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         
         statusBarMenu.addItem(
             withTitle: "Fetch Teecee Pic",
-            action: #selector(AppDelegate.fetchTeeceePic),
+            action: #selector(AppDelegate.fetchPuppyPics),
             keyEquivalent: "")
         
         statusBarMenu.addItem(
             withTitle: "Fetch Coder Pic",
-            action: #selector(AppDelegate.fetchCoderPic),
+            action: #selector(AppDelegate.fetchPuppyPics),
             keyEquivalent: "")
         
         statusBarMenu.addItem(
             withTitle: "Fetch Teece and Coder Pic",
-            action: #selector(AppDelegate.fetchTeeceeNCoderPic),
+            action: #selector(AppDelegate.fetchPuppyPics),
             keyEquivalent: "")
         
         statusBarMenu.addItem(
@@ -53,15 +55,30 @@ class AppDelegate: NSObject, NSApplicationDelegate {
             keyEquivalent: "")
     }
     
-    @objc func fetchTeeceePic(_ sender: NSMenuItem) {
+    
+    @objc func fetchPuppyPics(_ sender: NSMenuItem) {
         print( sender.title)
         let temporaryDirectoryURL = URL(fileURLWithPath: NSTemporaryDirectory(),
             isDirectory: true)
-  
+        if sender.title == "Fetch Teecee Pic" {
+            print("Hello")
+           api_endpoint = "teecee-pic"
+           puppy = "Teecee"
+            
+        } else if sender.title == "Fetch Coder Pic" {
+            api_endpoint = "coder-pic"
+            puppy = "Coder"
+            
+        } else {
+            api_endpoint = "teecee-coder-pic"
+            puppy = "teecee-coder"
+        }
+
+
         if Reachability.isConnectedToNetwork(){
             print("Internet Connection Available!")
             // Great we have internet now we can use our api to get the images from our s3 bucket
-            let url = URL(string: "https://vew5aom1hl.execute-api.us-east-1.amazonaws.com/dev/teecee-pic")
+            let url = URL(string: "https://vew5aom1hl.execute-api.us-east-1.amazonaws.com/dev/\(api_endpoint)")
             if let usableUrl = url {
                 let request = URLRequest(url: usableUrl)
                 let task = URLSession.shared.dataTask(with: request, completionHandler: { (data, response, error) in
@@ -80,9 +97,9 @@ class AppDelegate: NSObject, NSApplicationDelegate {
             let username = NSUserName()
             // print(username)
             do {
-                let files = try FileManager.default.contentsOfDirectory(atPath: "/Users/\(username)/Pictures/Teecee/")
+                let files = try FileManager.default.contentsOfDirectory(atPath: "/Users/\(username)/Pictures/\(puppy)/")
                 let myItem = files.randomElement()
-                    openPhotos(photoToOpen: "/Users/\(username)/Pictures/Teecee/" +  myItem!)
+                    openPhotos(photoToOpen: "/Users/\(username)/Pictures/\(puppy)/" +  myItem!)
                 print(files)
             } catch {
                 print(error)
@@ -101,123 +118,12 @@ class AppDelegate: NSObject, NSApplicationDelegate {
                 else { return }
             DispatchQueue.main.async() { () -> Void in
                 let fileManager = FileManager.default
-                let myFilePathString = stringofurl + "teecee.jpg"
+                let myFilePathString = stringofurl + "\(self.puppy).jpg"
                 fileManager.createFile(atPath: myFilePathString, contents: data, attributes: nil)
-                self.openPhotos(photoToOpen: stringofurl + "teecee.jpg")
+                self.openPhotos(photoToOpen: stringofurl + "\(self.puppy).jpg")
             }}.resume()
     }
-}
-
-    @objc func fetchCoderPic(_ sender: NSMenuItem) {
-        // print("Fetching Random Teecee Pic")
-        print( sender.title)
-        let temporaryDirectoryURL = URL(fileURLWithPath: NSTemporaryDirectory(),
-                                        isDirectory: true)
-        
-        if Reachability.isConnectedToNetwork(){
-            print("Internet Connection Available!")
-            // Great we have internet now we can use our api to get the images from our s3 bucket
-            let url = URL(string: "https://vew5aom1hl.execute-api.us-east-1.amazonaws.com/dev/coder-pic")
-            if let usableUrl = url {
-                let request = URLRequest(url: usableUrl)
-                let task = URLSession.shared.dataTask(with: request, completionHandler: { (data, response, error) in
-                    if let data = data {
-                        if let stringData = String(data: data, encoding: .utf8) {
-                            let phototoget = stringData.components(separatedBy: ",").randomElement()
-                            getPhotos(photoToOpen: phototoget!, tempurl: temporaryDirectoryURL)
-                        }
-                    }
-                })
-                task.resume()
-            }
-        }else{
-            print("Internet Connection not Available!")
-            // Here is we want to pull from our Pictures folder so that we can still show images even if offline
-            let username = NSUserName()
-            // print(username)
-            do {
-                let files = try FileManager.default.contentsOfDirectory(atPath: "/Users/\(username)/Pictures/Coder/")
-                let myItem = files.randomElement()
-                    openPhotos(photoToOpen: "/Users/\(username)/Pictures/Coder/" +  myItem!)
-                print(files)
-            } catch {
-                print(error)
-            }
-        }
-        
-        func getPhotos(photoToOpen: String,tempurl: URL) {
-            let stringofurl = tempurl.absoluteString.replacingOccurrences(of: "file:", with: "")
-            print(stringofurl)
-            let photoToOpen = photoToOpen.replacingOccurrences(of: "\"", with: "").replacingOccurrences(of: "]", with: "").replacingOccurrences(of: "[", with: "")
-            let url2 = URL(string: photoToOpen)
-            URLSession.shared.dataTask(with: url2!) { (data, response, error) in
-                guard
-                    let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
-                    let data = data, error == nil
-                    else { return }
-                DispatchQueue.main.async() { () -> Void in
-                    let fileManager = FileManager.default
-                    let myFilePathString = stringofurl + "coder.jpg"
-                    fileManager.createFile(atPath: myFilePathString, contents: data, attributes: nil)
-                    self.openPhotos(photoToOpen: stringofurl + "coder.jpg")
-                }}.resume()
-        }
-    }
-    
-    @objc func fetchTeeceeNCoderPic(_ sender: NSMenuItem) {
-        print(sender.title)
-        // print("Fetching Random Teecee Pic")
-        let temporaryDirectoryURL = URL(fileURLWithPath: NSTemporaryDirectory(),
-                                        isDirectory: true)
-        
-        if Reachability.isConnectedToNetwork(){
-            print("Internet Connection Available!")
-            // Great we have internet now we can use our api to get the images from our s3 bucket
-            let url = URL(string: "https://vew5aom1hl.execute-api.us-east-1.amazonaws.com/dev/teecee-coder-pic")
-            if let usableUrl = url {
-                let request = URLRequest(url: usableUrl)
-                let task = URLSession.shared.dataTask(with: request, completionHandler: { (data, response, error) in
-                    if let data = data {
-                        if let stringData = String(data: data, encoding: .utf8) {
-                            let phototoget = stringData.components(separatedBy: ",").randomElement()
-                            getPhotos(photoToOpen: phototoget!, tempurl: temporaryDirectoryURL)
-                        }
-                    }
-                })
-                task.resume()
-            }
-        }else{
-            print("Internet Connection not Available!")
-            // Here is we want to pull from our Pictures folder so that we can still show images even if offline
-            let username = NSUserName()
-            do {
-                let files = try FileManager.default.contentsOfDirectory(atPath: "/Users/\(username)/Pictures/teecee-coder/")
-                let myItem = files.randomElement()
-                    openPhotos(photoToOpen: "/Users/\(username)/Pictures/teecee-coder/" +  myItem!)
-            } catch {
-                print(error)
-            }
-        }
-        
-        func getPhotos(photoToOpen: String,tempurl: URL) {
-            let stringofurl = tempurl.absoluteString.replacingOccurrences(of: "file:", with: "")
-            print(stringofurl)
-            let photoToOpen = photoToOpen.replacingOccurrences(of: "\"", with: "").replacingOccurrences(of: "]", with: "").replacingOccurrences(of: "[", with: "")
-            let url2 = URL(string: photoToOpen)
-            URLSession.shared.dataTask(with: url2!) { (data, response, error) in
-                guard
-                    let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
-                    let data = data, error == nil
-                    else { return }
-                DispatchQueue.main.async() { () -> Void in
-                    let fileManager = FileManager.default
-                    let myFilePathString = stringofurl + "teecee-coder.jpg"
-                    fileManager.createFile(atPath: myFilePathString, contents: data, attributes: nil)
-                    self.openPhotos(photoToOpen: stringofurl + "teecee-coder.jpg")
-                }}.resume()
-        }
-    }
-    
+}    
     func openPhotos(photoToOpen: String) {
         NSWorkspace.shared.open(URL(fileURLWithPath: photoToOpen ))
     }
